6b144db44ff51d5292016c1df202ee2e5d11debd
Paulo Lopes <paulolsjbr@gmail.com>
2021-12-09 20:57:02 -0300

    Atualização de validações Validação do CPF

--- src/main/java/org/zup/paulo/gerComics/domain/Usuario.java
+++ src/main/java/org/zup/paulo/gerComics/domain/Usuario.java
@@ -1,13 +1,19 @@
 package org.zup.paulo.gerComics.domain;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.zup.paulo.gerComics.annotation.Cpf;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name="usuario")
@@ -23,4 +29,68 @@ public class Usuario implements Serializable {
     @NotBlank(message = "Nome é obrigatorio")
     private String nome;
 
+    @NotBlank(message = "E-mail é obrigatorio")
+    @Email(message = "E-mail invalido")
+    private String email;
+
+    @NotBlank(message = "CPF é obrigatorio")
+    @Cpf(message = "CPF invalido")
+    private String cpf;
+
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @NotNull(message = "Data de nascimento é obrigatoria")
+    private LocalDate dataNasc;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return getId().equals(usuario.getId()) && getNome().equals(usuario.getNome()) && getEmail().equals(usuario.getEmail()) && getCpf().equals(usuario.getCpf());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNome(), getEmail(), getCpf());
+    }
 }
